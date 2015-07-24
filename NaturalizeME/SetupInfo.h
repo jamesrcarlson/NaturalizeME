b289f9d9bf5b800f306ba6c96521b522eb900ccf
James Carlson <jamesrcarlson@gmail.com>
2015-07-23 21:22:28 -0600

    added API
    Added GoogleCivics API and removed several views. Now it is only a quiz app, no full test included

---
+++ NaturalizeME/SetupInfo.h
@@ -0,0 +1,20 @@
+//
+//  SetupInfo.h
+//  NaturalizeME
+//
+//  Created by James Carlson on 7/23/15.
+//  Copyright (c) 2015 JC2 Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface SetupInfo : NSManagedObject
+
+@property (nonatomic, retain) NSString * governnor;
+@property (nonatomic, retain) NSString * senatorOne;
+@property (nonatomic, retain) NSString * representative;
+@property (nonatomic, retain) NSString * senatorTwo;
+
+@end
---
+++ NaturalizeME/SetupInfo.m
@@ -0,0 +1,19 @@
+//
+//  SetupInfo.m
+//  NaturalizeME
+//
+//  Created by James Carlson on 7/23/15.
+//  Copyright (c) 2015 JC2 Dev. All rights reserved.
+//
+
+#import "SetupInfo.h"
+
+
+@implementation SetupInfo
+
+@dynamic governnor;
+@dynamic senatorOne;
+@dynamic representative;
+@dynamic senatorTwo;
+
+@end
